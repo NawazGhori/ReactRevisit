a8fa810c8d748fa363bc71ad18e3b450afc55c28
NawazGhori <mohammednawaz24@gmail.com>
2021-04-10 08:03:42 +0530

    Material Table,Mern Impl

--- components-app/src/rest-ex/CustRest.tsx
+++ components-app/src/rest-ex/CustRest.tsx
@@ -1,34 +1,76 @@
+// import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import axios from "axios";
-import React,{Component} from "react";
-interface CustRestProps{}
-interface CustRestState{
-    response:any
+import React, { Component } from "react";
+import Customers from "../model/Customers";
+interface CustRestProps { }
+interface CustRestState {
+    response: Customers,
+    rows: any
 }
 
-class CustRest extends Component<CustRestProps,CustRestState>{
-    constructor(props:CustRestProps){
+class CustRest extends Component<CustRestProps, CustRestState>{
+    constructor(props: CustRestProps) {
         super(props)
         this.state = {
-            response : {}
+            response: { records: [] },
+            rows: []
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get("https://www.w3schools.com/angular/customers.php").then(
-            (posRes)=>{
-                const {data} = posRes
+            (posRes) => {
+                const { data } = posRes
                 this.setState({
-                    response: data
+                    response: data,
+                    rows: data.records
                 })
-            },(errRes)=>{
+            }, (errRes) => {
                 console.log(errRes)
             })
-    }
 
-    render(){
-        return(
+        console.log(this.state.rows)
+    }
+    // useStyles = makeStyles({
+    //     table: {
+    //       minWidth: 650,
+    //     },
+    //   });
+    // classes = useStyles();
+    render() {
+        return (
             <React.Fragment>
-                {JSON.stringify(this.state.response)}
+                {/* {JSON.stringify(this.state.response)} */}
+
+                <TableContainer component={Paper}>
+                    <Table  className="table" aria-label="simple table">
+                        <TableHead >
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell align="right">City</TableCell>
+                                <TableCell align="right">Country</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.rows.map((r:any)=>( 
+                            <TableRow key={r.name}>
+                                <TableCell component="th" scope="row" >{r.Name}</TableCell>
+                                <TableCell align="right">{r.City}</TableCell>
+                                <TableCell align="right">{r.Country}</TableCell>
+                            </TableRow>
+                        ))}
+
+                        </TableBody>
+                    </Table>
+
+                </TableContainer>
             </React.Fragment>
         )
     }
